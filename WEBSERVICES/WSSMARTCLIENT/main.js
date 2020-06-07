e1fb035da52902681f42b5b193b88248b2e70733
unknown <flaviomaques85@gmail.com>
2020-06-07 17:22:51 -0300

    Client do WS de Clientes em Node.Js

--- WEBSERVICES/WSSMARTCLIENT/main.js
+++ WEBSERVICES/WSSMARTCLIENT/main.js
@@ -4,9 +4,8 @@ class Api{
 
     static async getCustomer(cod){
         const response = await axios.get('http://localhost:8089/rest/CLIENTES?CGCCLI=',{cod});
-        console.log('response: ', response);
-        
+        console.log('response: ', response); 
     }
 }
-
+//Chama o metodo GET passando o codigo como paramentro.
 Api.getCustomer('07139853000158');
